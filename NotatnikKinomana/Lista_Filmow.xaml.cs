551275e90f18f5b3bdf3bc4e1f6c7eefdd82c501
Helena <missalena99@outlook.com>
2019-05-12 14:24:41 +0200

    first

---
+++ NotatnikKinomana/Lista_Filmow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NotatnikKinomana
+{
+    /// <summary>
+    /// Логика взаимодействия для Lista_Filmow.xaml
+    /// </summary>
+    public partial class Lista_Filmow : Window
+    {
+        public Lista_Filmow()
+        {
+            InitializeComponent();
+        }
+    }
+}
